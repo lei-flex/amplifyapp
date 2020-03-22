dea4943d761b39c45c28c82136245719418ad317
Lei Zheng <zhenglei2010fall@gmail.com>
2020-03-21 22:18:45 -0400

    add runtime_env check

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> env: {process.env.NODE_ENV} and save to reload.
+          Edit <code>src/App.js</code> env: {process.env.NODE_ENV}, cust env: {process.env.RUNTIME_ENV} and save to reload.
         </p>
         <a
           className="App-link"
